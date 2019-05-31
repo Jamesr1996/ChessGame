f9bf8f09ac3f6b442c23332946dc1b86f5068559
James Richardson <Jamesr_1996@hotmail.co.uk>
2019-05-31 17:32:15 +0100

    Players added
    Set Player names and colours upon starting the game.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Enter Player 1 name: ");
+		Player player1 = new Player(in.nextLine(), "White");
+		
+		System.out.println("Enter Player 2 name: ");
+		Player player2 = new Player("Enter Player 2 name: ", "Black");
+		
+		// Can add option to change character colours using player.setColour("White/Black");
+		System.out.println("Who wants to start?: ");
+		
 		ChessBoard board = new ChessBoard();
 		
 		board.displayBoard();
---
+++ src/Player.java
@@ -0,0 +1,13 @@
+public class Player {
+	public String name;
+	public String colour;
+	
+	public Player(String name, String colour){
+		this.name = name;
+		this.colour = colour;
+	}
+	
+	void setPlayerColour(String colour){
+		this.colour = colour;
+	}
+}
